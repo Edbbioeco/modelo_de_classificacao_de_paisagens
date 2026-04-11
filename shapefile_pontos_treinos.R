866d033c832e3f8a5bcb81da6fa26b103c77ed60
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2026-04-11 19:25:20 -0300

    Baixados os shapefiles da região metropolitana do Recife

--- shapefile_pontos_treinos.R
+++ shapefile_pontos_treinos.R
@@ -13,3 +13,26 @@ library(terra)
 library(mapedit)
 
 library(sf)
+
+# Dados ----
+
+## Região metropolitana de Recife ----
+
+### Importando ----
+
+rmr <- geobr::read_municipality(year = 2019) |>
+  dplyr::filter(name_muni %in% c("Abreu e Lima",
+                                 "Araçoiaba",
+                                 "Cabo de Santo Agostinho",
+                                 "Camaragibe",
+                                 "Igarassu",
+                                 "Ipojuca",
+                                 "Itamaracá",
+                                 "Itapissuma",
+                                 "Jaboatão dos Guararapes",
+                                 "Moreno",
+                                 "Olinda",
+                                 "Paulista",
+                                 "Recife",
+                                 "São Lourenço da Mata"))
+
