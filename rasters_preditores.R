ece0a284a8522508f44fe6340514af376f685b68
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2026-04-11 20:31:32 -0300

    Importar os shapefiles dos municípios da Região Metropolitana de Recife

--- rasters_preditores.R
+++ rasters_preditores.R
@@ -13,3 +13,33 @@ library(sf)
 library(maptiles)
 
 library(rgee)
+
+# Dados ----
+
+## Shapefiles dos municípios da Região Metropolitana do Recife
+
+### Importado ----
+
+rmr <- geobr::read_municipality(year = 2019) |>
+  dplyr::filter(name_muni %in% c("Abreu E Lima",
+                                 "Araçoiaba",
+                                 "Cabo De Santo Agostinho",
+                                 "Camaragibe",
+                                 "Igarassu",
+                                 "Ipojuca",
+                                 "Ilha De Itamaracá",
+                                 "Itapissuma",
+                                 "Jaboatão Dos Guararapes",
+                                 "Moreno",
+                                 "Olinda",
+                                 "Paulista",
+                                 "Recife",
+                                 "São Lourenço Da Mata") &
+                  abbrev_state == "PE")
+
+### Visualizando ----
+
+rmr
+
+ggplot() +
+  geom_sf(data = rmr, color = "black")
