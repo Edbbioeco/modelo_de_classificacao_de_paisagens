03c74a617981bf5ac20600ee1d5feed8b6d00f39
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2026-04-12 13:15:19 -0300

    Importar e visualizar o raster de imagem de satélite

--- modelo_de_classificacao.R
+++ modelo_de_classificacao.R
@@ -30,3 +30,18 @@ pontos
 ggplot() +
   geom_sf(data = pontos, aes(color = Class)) +
   scale_color_viridis_d()
+
+## Raster de imagem de satélite ----
+
+### Importar ----
+
+img_sat <- terra::rast("img_sat.tif")
+
+### Visualizar ----
+
+img_sat
+
+ggplot() +
+  tidyterra::geom_spatraster_rgb(data = img_sat) +
+  geom_sf(data = pontos, aes(color = Class)) +
+  scale_color_viridis_d()
