9443641546b0c753e26ffcd9a2c2f579ec44d9cf
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2026-04-15 17:32:47 -0300

    Visualizar raster de predição

--- area_fragmentos.R
+++ area_fragmentos.R
@@ -14,3 +14,10 @@ library(sf)
 
 predicao <- terra::rast("predicao.tif")
 
+## Visualizar ----
+
+predicao
+
+ggplot() +
+  tidyterra::geom_spatraster(data = predicao) +
+  scale_fill_viridis_d(na.translate = FALSE)
